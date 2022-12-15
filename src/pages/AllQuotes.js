c3ddfb8a08763d6f478e461c55b471aa4e05e255
dawidlehai <haidawidle@gmail.com>
2022-12-15 18:15:40 +0100

    Added dummy quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,20 @@
+import { Fragment } from "react";
+
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Dawid", text: "Healing comes with time" },
+  { id: "q2", author: "Max", text: "Learning React is fun!" },
+  { id: "q3", author: "Daniel", text: "I care, but most of the time I don't" },
+];
+
 const AllQuotes = () => {
-  return <h1>All quotes page</h1>;
+  return (
+    <Fragment>
+      <h1>All quotes</h1>
+      <QuoteList quotes={DUMMY_QUOTES} />
+    </Fragment>
+  );
 };
 
 export default AllQuotes;
