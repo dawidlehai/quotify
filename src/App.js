0096e6246bea2ba6aaff7ea899e39963053cc689
dawidlehai <haidawidle@gmail.com>
2022-12-15 17:26:21 +0100

    Added BrowserRouter which fixes blank route pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import QuoteDetails from "./pages/QuoteDetails";
 function App() {
   return (
     <Switch>
-      <Route path="/quotes">
+      <Route path="/quotes" exact>
         <AllQuotes />
       </Route>
       <Route path="/quotes/:quoteId">
